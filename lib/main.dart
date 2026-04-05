8d1cb61d6617d883dbf66e8e4875aadae6239c74
Khaled kammoun <139220585+khaled214566@users.noreply.github.com>
2026-04-05 15:37:43 +0100

    small improvments

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:idgaf/core/configs/theme/app_theme.dart';
 import 'package:idgaf/presentation/choose_mode/bloc/theme_cubit.dart';
-import 'package:idgaf/presentation/splash/pages/splash.dart';
+import 'package:idgaf/presentation/splash/pages/opening_page.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
